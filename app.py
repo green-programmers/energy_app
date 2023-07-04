8b0d7fa824f659ddaf0d4db4fda986ab9c115035
And <78680782+And-oss@users.noreply.github.com>
2023-07-04 19:37:09 +0300

    Basic functional

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+from web_part import route,other
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route(route.main)
 def main_page():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True ,port=other.server_port,ssl_context=other.server_ssl)
---
+++ web_part/other.py
@@ -0,0 +1,8 @@
+server_port = 45600
+server_ip = "localhost"
+server_ssl = "adhoc"
+
+db_host = "localhost"
+db_port = 5432
+db_name = "postgres"
+db_user = "postgres"
