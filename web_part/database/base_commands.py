5cf2082a612e4051ea1a8bf89ebc8b808c6db1bd
And <78680782+And-oss@users.noreply.github.com>
2023-07-05 19:50:12 +0300

    class with PEP8

--- web_part/database/base_commands.py
+++ web_part/database/base_commands.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 class BasicCommands:
-    def __init__(self,dbname , username , password ,connection=None,cursor = None):
+    def __init__(self, dbname, username, password, connection=None, cursor=None):
         self.dbname = dbname
         self.username = username
         self.password = password
@@ -11,20 +11,21 @@ class BasicCommands:
     def connect(self):
         self.connection = psycopg2.connect(f"dbname={self.dbname} user={self.username} password={self.password}")
         self.cursor = self.connection.cursor()
-    def select_query(self,table):
+
+    def select_query(self, table):
         self.cursor.execute(f"SELECT * FROM {table}")
 
     def fetch_all(self):
         fetch = self.cursor.fetchall()
         print(fetch)
 
-    def insert_into(self , table , *args):
+    def insert_into(self, table, *args):
         try:
             insert = f"""INSERT INTO {table} VALUES {args}"""
             self.cursor.execute(insert)
             self.connection.commit()
         except Exception as e:
-            print("[ERROR] ",e)
+            print("[ERROR] ", e)
 
     def close_connection(self):
         self.cursor.close()
