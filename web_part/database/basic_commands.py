14545d0fa2a2add59037fe1e4c2ef3a912c86375
And <78680782+And-oss@users.noreply.github.com>
2023-07-05 12:17:51 +0300

    Add basic functions for BasicCommands class

--- web_part/database/basic_commands.py
+++ web_part/database/basic_commands.py
@@ -1,20 +1,30 @@
 import psycopg2
 
-class basic_commands:
-    def __init__(self,dbname , username , password ,cursor = None):
+class BasicCommands:
+    def __init__(self,dbname , username , password ,connection=None,cursor = None):
         self.dbname = dbname
         self.username = username
         self.password = password
+        self.connection = connection
         self.cursor = cursor
+
     def connect(self):
-        self.cursor = psycopg2.connect(f"dbname={self.dbname} user={self.username} password={self.password}").cursor()
+        self.connection = psycopg2.connect(f"dbname={self.dbname} user={self.username} password={self.password}")
+        self.cursor = self.connection.cursor()
     def select_query(self,table):
         self.cursor.execute(f"SELECT * FROM {table}")
-    def fetch_all(self , num_of_rows):
-        fetch = self.cursor.fetchall()
-        inc = 0
 
-        for row in fetch:
-            while inc < num_of_rows:
-                print(row[inc])
-                inc+=1
+    def fetch_all(self):
+        fetch = self.cursor.fetchall()
+        print(fetch)
+
+    def insert_into(self , table , *args):
+        try:
+            insert = f"""INSERT INTO {table} VALUES {args}"""
+            self.cursor.execute(insert)
+            self.connection.commit()
+        except Exception as e:
+            print("[ERROR] ",e)
+
+    def close_connection(self):
+        self.cursor.close()
