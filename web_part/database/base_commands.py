0015355412f4ecd99b814e3518f1ce1fce3a622c
Ekeleni <99543058+Ekeleni@users.noreply.github.com>
2023-07-05 04:50:09 +0300

    pep8

--- web_part/database/base_commands.py
+++ web_part/database/base_commands.py
@@ -1,19 +1,21 @@
 import psycopg2
 
-class basic_commands:
+class BasicCommands:
     def __init__(self,dbname , username , password ,cursor = None):
         self.dbname = dbname
         self.username = username
         self.password = password
         self.cursor = cursor
+
     def connect(self):
         self.cursor = psycopg2.connect(f"dbname={self.dbname} user={self.username} password={self.password}").cursor()
+
     def select_query(self,table):
         self.cursor.execute(f"SELECT * FROM {table}")
+
     def fetch_all(self , num_of_rows):
         fetch = self.cursor.fetchall()
         inc = 0
-
         for row in fetch:
             while inc < num_of_rows:
                 print(row[inc])
